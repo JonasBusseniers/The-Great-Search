a9a162cc426d8aa100d31993f671da4e22a946a2
Jonas Busseniers <j_busseniers@hotmail.com>
2019-01-19 00:22:11 +0100

    coords stage2

--- database/stage2.js
+++ database/stage2.js
@@ -4,8 +4,8 @@ var locations = [
       "found": false,
       "image":"images/stage2/1.jpg",
       "coords": {
-        "lat": 51.183,
-        "long": 3.576
+        "lat": 51.053766,
+        "long": 3.730581
       }
     },
     {
@@ -13,64 +13,64 @@ var locations = [
       "found": false,
       "image":"images/stage2/2.jpg",
       "coords": {
-        "lat": 51.183,
-        "long": 3.576
+        "lat": 51.054621,
+        "long": 3.729363
       }
     },    {
       "id": "2-3",
       "found": false,
       "image":"images/stage2/3.jpg",
       "coords": {
-        "lat": 51.183,
-        "long": 3.576
+        "lat": 51.054350,
+        "long": 3.727927
       }
     },    {
       "id": "2-4",
       "found": false,
       "image":"images/stage2/4.jpg",
       "coords": {
-        "lat": 51.183,
-        "long": 3.576
+        "lat": 51.053891,
+        "long": 3.730786
       }
     },    {
       "id": "2-5",
       "found": false,
       "image":"images/stage2/5.jpg",
       "coords": {
-        "lat": 51.183,
-        "long": 3.576
+        "lat": 51.053422,
+        "long": 3.725658
       }
     },    {
       "id": "2-6",
       "found": false,
       "image":"images/stage2/6.jpg",
       "coords": {
-        "lat": 51.183,
-        "long": 3.576
+        "lat": 51.054906,
+        "long": 3.728958
       }
     },    {
       "id": "2-7",
       "found": false,
       "image":"images/stage2/7.jpg",
       "coords": {
-        "lat": 51.183,
-        "long": 3.576
+        "lat": 51.053915,
+        "long": 3.728245
       }
     },    {
       "id": "2-8",
       "found": false,
       "image":"images/stage2/8.jpg",
       "coords": {
-        "lat": 51.183,
-        "long": 3.576
+        "lat": 51.054546,
+        "long": 3.729280
       }
     },    {
       "id": "2-9",
       "found": false,
       "image":"images/stage2/9.jpg",
       "coords": {
-        "lat": 51.183,
-        "long": 3.576
+        "lat": 51.053710,
+        "long": 3.727923
       }
     }
   ]
